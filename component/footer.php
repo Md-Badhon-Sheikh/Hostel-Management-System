d700d6a64c4c39ae44fd258a33faf60a59b0fed2
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-12-28 23:03:35 +0600

    added more

--- component/footer.php
+++ component/footer.php
@@ -1,102 +1,40 @@
-<footer class="bg-[--primary-color] py-5 px-4 shadow-xl shadow-cyan-300">
-  <section class="max-w-7xl mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-5">
-    <!-- ======== footer left content ====== -->
-    <div class="">
-      <div class="footer_logo cursor-pointer flex justify-center md:justify-start">
-        <a href="home.php">
-            <img src="../images/footer_logo.webp" alt="footer logo">
-        </a>
-      </div>
-      <p class="py-3 md:text-start text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Perferendis
-        odit
-        officiis
-        molestias temporibus
-        ipsam.
-      </p>
-      <div class="readbtn mt-2 text-center md:text-start">
-        <!-- <a href="#" class="px-5 py-2 border-2 rounded-lg border-blue-500">Read More</a> -->
-        <a href="#"
-          class="relative inline-block px-5 py-2 border-2 rounded-lg border-blue-500 hover:text-white overflow-hidden group">
-          <span
-            class="absolute inset-0 bg-blue-500 translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-300 ease-out"></span>
-          <span class="relative z-10">Read More</span>
-        </a>
-
-      </div>
-    </div>
-
-    <!-- ========== footer middel content ======= -->
-    <div class="text-center">
-      <h3 class="uppercase text-2xl">Connect With Us</h3>
-      <p class="py-3">SOCIAL MEDIA CHANNELS</p>
-      <div class="social_box flex justify-center items-center gap-2">
-        <a href="">
-          <img src="<?php echo '/hotelix_hotel_management/assets/footer_social_logo/facebook.png' ?>"
-            alt="facebook">
-        </a>
-        <a href="">
-          <img src="<?php echo '/hotelix_hotel_management/assets/footer_social_logo/youtube.png' ?>"
-            alt="facebook">
-        </a>
-        <a href="">
-          <img src="<?php echo '/hotelix_hotel_management/assets/footer_social_logo/instagram.png' ?>"
-            alt="facebook">
-        </a>
-        <a href="">
-          <img src="<?php echo '/hotelix_hotel_management/assets/footer_social_logo/media.png' ?>"
-            alt="facebook">
-        </a>
-        <a href="">
-          <img src="<?php echo '/hotelix_hotel_management/assets/footer_social_logo/twitter.png' ?>"
-            alt="facebook">
-        </a>
-      </div>
-    </div>
-
-    <!-- ======= footer right content ======= -->
-    <div class="">
-      <h3 class="uppercase">Subscribe To Receive News Updates & Offers</h3>
-      <p class="py-3">SIGN UP FOR SPECIAL OFFERS</p>
-      <form action="">
-        <input type="email" id="email" name="email" placeholder="insert your email"
-          class="w-full outline-none bg-[--primary-color] rounded-md shadow-md shadow-blue-500 ps-4 py-3">
-        <input type="submit" value="Subscribe Us"
-          class="border-2 border-y-blue-500 border-x-blue-500 w-full p-4 my-4 hover:bg-green-600 text-[--serondary-color] uppercase duration-100 transition-all hover:text-white rounded-sm">
-      </form>
-    </div>
-
-  </section>
+<footer class="footer bg-base-200 text-base-content p-10">
+  <aside>
+    <svg
+      width="50"
+      height="50"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      class="fill-current">
+      <path
+        d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+    </svg>
+    <p>
+      ACME Industries Ltd.
+      <br />
+      Providing reliable tech since 1992
+    </p>
+  </aside>
+  <nav>
+    <h6 class="footer-title">Services</h6>
+    <a class="link link-hover">Branding</a>
+    <a class="link link-hover">Design</a>
+    <a class="link link-hover">Marketing</a>
+    <a class="link link-hover">Advertisement</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Company</h6>
+    <a class="link link-hover">About us</a>
+    <a class="link link-hover">Contact</a>
+    <a class="link link-hover">Jobs</a>
+    <a class="link link-hover">Press kit</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Legal</h6>
+    <a class="link link-hover">Terms of use</a>
+    <a class="link link-hover">Privacy policy</a>
+    <a class="link link-hover">Cookie policy</a>
+  </nav>
 </footer>
-<!-- == privacy == -->
-<div class="md:grid grid-cols-2 py-3 md:px-10 px-4 bg-gray-800 text-white">
-  <div class="flex gap-8">
-    <a href=""
-      class=" border-r-2 border-gray-800 hover:border-white rounded-sm transition-all p-2 relative inline-block overflow-hidden group">
-      <span
-        class="absolute inset-0 bg-blue-500 translate-x-[-110%] group-hover:translate-x-0 transition-transform duration-300 ease-out"></span>
-      <span class="relative z-10">Home</span>
-    </a>
-    <a href=""
-      class="border-r-2 border-gray-800 hover:border-white rounded-sm transition-all p-2 relative inline-block overflow-hidden group">
-      <span
-        class="absolute inset-0 bg-blue-500 translate-x-[-110%] group-hover:translate-x-0 transition-transform duration-300 ease-out"></span>
-      <span class="relative z-10">About</span>
-    </a>
-    <a href=""
-      class="border-r-2 border-gray-800 hover:border-white rounded-sm transition-all p-2 relative inline-block overflow-hidden group">
-      <span
-        class="absolute inset-0 bg-blue-500 translate-x-[-110%] group-hover:translate-x-0 transition-transform duration-300 ease-out"></span>
-      <span class="relative z-10">Gallery</span>
-    </a>
-    <a href=""
-      class="border-r-2 border-gray-800 hover:border-white rounded-sm transition-all p-2 relative inline-block overflow-hidden group">
-      <span
-        class="absolute inset-0 bg-blue-500 translate-x-[-110%] group-hover:translate-x-0 transition-transform duration-300 ease-out"></span>
-      <span class="relative z-10">Contact</span>
-    </a>
-  </div>
-  <div class="flex items-center md:justify-end justify-center">
-    <span>Copyright © <?php echo date('Y'); ?> Hotelix. All Rights Reserved.</span>
-  </div>
-</div>
