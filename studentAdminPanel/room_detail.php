1900ec406dc26369de6eeb3d1cba7aad2637b7bb
Md-Badhon-Sheikh <badhon0854@gmail.com>
2025-01-02 10:29:31 +0600

    added student admin panel

---
+++ studentAdminPanel/room_detail.php
@@ -0,0 +1,53 @@
+<?php
+include_once('../db_root.php');
+session_start();
+
+if (!isset($_SESSION['mySession'])) {
+  header('location:../index.php');
+}
+
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" href="img/fav.png" type="image/x-icon">
+  <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.12.1/css/pro.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Welcome To Cleopatra</title>
+</head>
+
+<body class="bg-gray-100">
+
+
+  <!-- start navbar -->
+  <?php
+      include ('shared/header.php');
+  ?>
+  <!-- end navbar -->
+
+
+  <!-- strat wrapper -->
+  <div class="h-screen flex flex-row flex-wrap">
+
+    <!-- start sidebar -->
+    <?php
+    include('shared/sidebar.php')
+    ?>
+    <!-- end side bar -->
+
+
+  </div>
+  <!-- end wrapper -->
+
+  <!-- script -->
+  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+  <script src="js/scripts.js"></script>
+  <!-- end script -->
+
+</body>
+
+</html>
